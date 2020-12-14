2e27c891e11b6676b8cc3f74121dda1d270b7027
Coulibaly <Coulibaly60.sc@gmail.com>
2020-12-14 01:59:39 +0000

    recuperation totale des données

--- projet02.py
+++ projet02.py
@@ -4,8 +4,16 @@ from bs4 import BeautifulSoup
 
 url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 resultat = requests.get(url)
-
+list1 = []
 if resultat.ok:
     soup = BeautifulSoup(resultat.text, 'lxml')
     titre = soup.find('h1')
     print(titre.text)
+    data = soup.find_all('th')
+
+    data1 = soup.find_all('td')
+
+    for i in data:
+        print(i.string)
+    for j in data1:
+        print(j.string)
