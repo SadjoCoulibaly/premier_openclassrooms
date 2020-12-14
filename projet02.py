69d9e63ddbe829bd1d20262251529a5e504bbfed
Coulibaly <Coulibaly60.sc@gmail.com>
2020-12-14 21:20:01 +0000

    inserer dans un liste???

--- projet02.py
+++ projet02.py
@@ -5,19 +5,27 @@ import csv
 url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 resultat = requests.get(url)
 liste = []
+liste1 = []
 if resultat.ok:
     soup = BeautifulSoup(resultat.text, 'lxml')
-    titre = soup.find('h1')
-    f = open('donnee.csv', 'w')
-    with f:
-        writer = csv.writer(f)
-        writer.writerow(titre)
+
+    data = soup.find_all('th', text='')
+    liste.append(data)
+    data2 = soup.find_all('td', text='')
+    liste1.append(data2)
+
+    for i in liste:
+        print(i)
+        for j in liste1:
+            print(j)
 
     """data = soup.find_all('th')
     for i in data:
         print(i.string)
+
     data1 = soup.find_all('td')
     for j in data1:
         print(j.string)"""
+
 
 
