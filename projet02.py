a1a1d41b39a2f0ce5bfe666a0496cc963f23cf4f
Coulibaly <Coulibaly60.sc@gmail.com>
2020-12-14 01:28:22 +0000

    recuperation du titre de la page avec succes

--- projet02.py
+++ projet02.py
@@ -1,3 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+resultat = requests.get(url)
+
+if resultat.ok:
+    soup = BeautifulSoup(resultat.text, 'lxml')
+    titre = soup.find('h1')
+    print(titre.text)
