7e274c2d2cf1f670d8aed7546bb6f81054784cef
Coulibaly <Coulibaly60.sc@gmail.com>
2020-12-14 20:39:00 +0000

    recuperation totale des données version(2)

--- projet02.py
+++ projet02.py
@@ -4,18 +4,14 @@ import csv
 
 url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 resultat = requests.get(url)
-liste = {}
+liste = []
 if resultat.ok:
     soup = BeautifulSoup(resultat.text, 'lxml')
     titre = soup.find('h1')
 
     data = soup.find_all('th')
     for i in data:
-        l1 = [liste.append(i.string)]
-
+        print(i.string)
     data1 = soup.find_all('td')
     for j in data1:
-        l2 = [liste.append(j.string)]
-
-liste = {l1: l2}
-print(liste)
+        print(j.string)
