19f4b9690b34c40d6e74497a9f0bd6626ecac2bd
Coulibaly <Coulibaly60.sc@gmail.com>
2020-12-14 03:09:08 +0000

    entrer les données dans un dictionnaire

--- projet02.py
+++ projet02.py
@@ -1,19 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-
+import csv
 
 url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
 resultat = requests.get(url)
-list1 = []
+liste = {}
 if resultat.ok:
     soup = BeautifulSoup(resultat.text, 'lxml')
     titre = soup.find('h1')
-    print(titre.text)
+
     data = soup.find_all('th')
+    for i in data:
+        l1 = [liste.append(i.string)]
 
     data1 = soup.find_all('td')
-
-    for i in data:
-        print(i.string)
     for j in data1:
-        print(j.string)
+        l2 = [liste.append(j.string)]
+
+liste = {l1: l2}
+print(liste)
