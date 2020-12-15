0976b3c62c5b06bd32b1851402ab0bd5881aeb30
Coulibaly <Coulibaly60.sc@gmail.com>
2020-12-15 01:26:56 +0000

    recuperation dans une liste et ecriture dans un csv (pas au point)

--- projet02.py
+++ projet02.py
@@ -10,14 +10,36 @@ if resultat.ok:
     soup = BeautifulSoup(resultat.text, 'lxml')
 
     data = soup.find_all('th', text='')
-    liste.append(data)
-    data2 = soup.find_all('td', text='')
-    liste1.append(data2)
+    print(data)
 
+    data1 = soup.find_all('td', text='')
+    print(data1)
+
+    with open('donnee.csv', 'w') as fichier:
+        writer = csv.writer(fichier)
+        for row in data:
+            writer.writerow(row)
+
+
+
+    """for i in data:
+        liste.append(i)
+        while i != len(liste):
+            print(liste)"""
+
+    """data2 = soup.find_all('td', text='')
+    liste1.append(data2)
+    t = len(liste1)
+    print(t)
     for i in liste:
         print(i)
         for j in liste1:
             print(j)
+    i = 0
+    j = 0
+    while i in len(liste):
+        while j in len(liste1):
+            print(liste[i], ':', liste1[j])"""
 
     """data = soup.find_all('th')
     for i in data:
@@ -26,6 +48,3 @@ if resultat.ok:
     data1 = soup.find_all('td')
     for j in data1:
         print(j.string)"""
-
-
-
